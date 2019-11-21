0ecf5c207a8e06e361cf8cf4fa06e500eeee641a
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2019-11-21 20:15:03 -0300

    testando calculos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BankList, banksExample } from './shared/entities/bank.entities';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  banks;
+  stock: any[];
+  bankList: BankList;
+
+  constructor() {
+    this.bankList = new BankList(banksExample);
+    console.log(JSON.stringify(this.bankList));
+
+    this.banks = this.bankList.banks.map(x=> x.name);
+    this.stock = this.bankList.banks.map(x=> x.stockAmount);
+  }
 }
---
+++ src/app/shared/entities/bank.entities.ts
@@ -0,0 +1,50 @@
+export interface Bank {
+    // nome
+    name: string;
+
+    // valor das ações
+    stockAmount: number;
+
+    // CDB
+    depositaryReceiptAmount: number;
+
+    // Poupança
+    savingsAmount: number;
+
+    treasuryBondsAmount: number;
+
+    accountBalance: number;
+}
+
+export const  banksExample =  [
+    { name: 'inter', stockAmount: 20,           depositaryReceiptAmount: 33220.61,  savingsAmount: 0,       treasuryBondsAmount: 0,         accountBalance: 301.83},
+    { name: 'bradesco', stockAmount: 0,         depositaryReceiptAmount: 0,         savingsAmount: 0,       treasuryBondsAmount: 0,         accountBalance: 0},
+    { name: 'easynvest', stockAmount: 2419.52,  depositaryReceiptAmount: 0,         savingsAmount: 1674.54, treasuryBondsAmount: 9275.14,   accountBalance: 10.50},
+    { name: 'modal', stockAmount: 3338.87,      depositaryReceiptAmount: 0,         savingsAmount: 0,       treasuryBondsAmount: 0,         accountBalance: 16.03},
+] as Bank[];
+
+
+
+export class BankList {   
+    constructor (public banks: Bank[]){
+
+    }
+
+    private sum(selectField: (b: Bank) => number) : number{
+        let sum = 0;
+        this.banks.forEach(b => sum += selectField(b));
+        return sum;
+    }
+
+    get stockAmount() { return this.sum(b => b.stockAmount); }
+    get depositaryReceiptAmount() { return this.sum(b => b.depositaryReceiptAmount); }
+    get savingsAmount() { return this.sum(b => b.savingsAmount); }
+    get treasuryBondsAmount() { return this.sum(b => b.treasuryBondsAmount); }
+    get accountBalance() { return this.sum(b => b.accountBalance); }
+
+    get total () {
+        let sum = 0;
+        this.banks.forEach(b => sum += (b.stockAmount + b.depositaryReceiptAmount + b.savingsAmount + b.treasuryBondsAmount + b.accountBalance ));
+        return sum;
+    }
+}
